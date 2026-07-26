6945548a4abfc21a95533d1865ab4fc0c002453f
Paweł Dubowik <p.dubowik27@gmail.com>
2026-07-26 21:54:46 +0200

    project init

---
+++ src/components/WeatherBox/WeatherBox.js
@@ -0,0 +1,51 @@
+import PickCity from '../PickCity/PickCity';
+import WeatherSummary from '../WeatherSummary/WeatherSummary';
+import Loader from '../Loader/Loader';
+import ErrorBox from '../ErrorBox/ErrorBox';
+import { useCallback, useState } from 'react';
+
+const WeatherBox = props => {
+
+  const [weather, setWeather] = useState(null);
+
+  const [pending, setPending] = useState(false);
+
+  const [error, setError] = useState(false);
+
+  const handleCityChange = useCallback(city => {
+    setError(false);
+
+    setPending(true);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3ea30e2c82e0f849652eef72d490c041&units=metric`)
+      .then(res => {
+        if(res.status === 200) {
+
+          return res.json()
+          .then(data => {
+            const weatherData = {
+              city: data.name,
+              temp: data.main.temp,
+              icon: data.weather[0].icon,
+              description: data.weather[0].main
+            };
+    
+            setWeather(weatherData);
+            setPending(false);
+          })
+      } else {
+        setError(true);
+      }
+  }, [])
+});
+
+  return (
+    <section>
+      <PickCity action={handleCityChange} />
+      {(weather && !pending) && <WeatherSummary weather={weather} />}
+      {error && <ErrorBox />}
+      {(pending && !error) && <Loader />}
+    </section>
+  )
+};
+
+export default WeatherBox;
---
+++ src/components/WeatherSummary/WeatherSummary.js
@@ -0,0 +1,25 @@
+import styles from './WeatherSummary.module.scss';
+
+const WeatherSummary = ({ weather }) => {
+
+  if(!weather) {
+    return null;
+  }
+
+  return (
+    <section className={styles.weatherSummary}>
+      <img
+        className={styles.weatherIcon}
+        alt={weather.description}
+        src={`${process.env.PUBLIC_URL}/images/weather-icons/${weather.icon}.png`} />
+      <div className={styles.weatherInfo}>
+        <h2>{weather.city}</h2>
+        <p>
+          <strong>Temp:</strong> {weather.temp}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default WeatherSummary;
